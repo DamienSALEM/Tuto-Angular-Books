539c74d592049ca6b37bb11e459b9b53068e928f
Damien SALEM <d.salem@ecole-ipssi.net>
2020-03-11 12:59:26 +0100

    Initialisation du projet et de firebase

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { SignupComponent } from "./auth/signup/signup.component";
+import { SigninComponent } from "./auth/signin/signin.component";
+import { BookListComponent } from "./book-list/book-list.component";
+import { SingleBookComponent } from "./book-list/single-book/single-book.component";
+import { BookFormComponent } from "./book-list/book-form/book-form.component";
+import { AuthService } from "./services/auth.service";
+import { AuthGuardService } from "./services/auth-guard.service";
+import { BooksService } from "./services/books.service";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "auth/signup", component: SignupComponent },
+  { path: "auth/signin", component: SigninComponent },
+  { path: "books", component: BookListComponent },
+  { path: "books/new", component: BookFormComponent },
+  { path: "books/view/:id", component: SingleBookComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as firebase from "firebase";
+import { config } from "process";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'book-application';
+  title = "book-application";
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyD5B31ZDfGOMbWLZgrMtkYDYAdY2dXXnGA",
+      authDomain: "books-db-870a3.firebaseapp.com",
+      databaseURL: "https://books-db-870a3.firebaseio.com",
+      projectId: "books-db-870a3",
+      storageBucket: "books-db-870a3.appspot.com",
+      messagingSenderId: "707226057951",
+      appId: "1:707226057951:web:07383a60b3a10d0dda3d6a",
+      measurementId: "G-BXJZ2ELH3J"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
