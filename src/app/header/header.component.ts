e7b5c538cfd04827a9a0ef91c886e8f7ef078963
Damien SALEM <d.salem@ecole-ipssi.net>
2020-03-11 13:57:12 +0100

    Ajout du système d'utilisateur (connexion/deconnexion) et vérification avec la DB.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  isAuth: boolean;
 
-  constructor() { }
+  constructor(private AuthService: AuthService) {}
 
   ngOnInit(): void {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.isAuth = true;
+      } else {
+        this.isAuth = false;
+      }
+    });
   }
 
+  onSignOut() {
+    this.AuthService.signOutUser();
+  }
 }
